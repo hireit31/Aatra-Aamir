6e7e21eb8c3810f5ed6e265e856f6c5d07976c3e
hamna4503 <hamnaaamir593@gmail.com>
2025-07-07 11:54:54 +0500

    Minor client changes
    Signed-off-by: hamna4503 <hamnaaamir593@gmail.com>

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -70,7 +70,7 @@ const About = () => {
       {/* Selected Clients */}
       <div className="mt-16">
         <h3 className="text-2xl md:text-4xl md:text-center font-bold md:mb-6 font-aboutHeading">
-          Selected clients
+          Featured clients
         </h3>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-5 md:mt-10 justify-center justify-items-center">
           {clientLogos.map((logo, index) => (
